8de1cc956727a5507e8c3929b283f5f548146190
pajaR_19 <53967072+paja-R@users.noreply.github.com>
2020-09-15 18:18:25 +0700

    Create run.php

---
+++ run.php
@@ -0,0 +1,16 @@
+<?php
+//https://stackoverflow.com/questions/61577868/read-json-with-php-from-instagram-a-1
+//github.com/paja-R
+
+$user = "pajaar1337";
+$link = "https://instagram.com/".$user."?__a=1";
+//decode json
+$json = json_decode(file_get_contents($link));
+
+//parse json
+$link = $json->graphql->user->profile_pic_url_hd;
+
+echo "<h1>User: $user";
+echo "<br><br>";
+echo "<img src=$link>";
+?>
